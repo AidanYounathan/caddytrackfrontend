76e854131671ee82da1f684dd9a17871928193e1
mkheck13 <mkheck13@gmail.com>
2024-04-11 14:38:43 -0700

    changed a c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
