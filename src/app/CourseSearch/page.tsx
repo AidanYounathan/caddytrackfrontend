259d5713de627345f97bc6e22c145e7b551f8a10
Girthstorm <ayounathan05@gmail.com>
2024-05-13 13:35:27 -0700

    Stuffin

--- src/app/CourseSearch/page.tsx
+++ src/app/CourseSearch/page.tsx
@@ -3,11 +3,12 @@
 import NavbarComponent from '@/Components/NavBarComponent'
 import CourseSearchComponent from '@/Components/CourseSearchComponent'
 import React from 'react'
+import '@/app/CourseSearch/styles.css'
 
 const Page = () => {
   return (
     <>
-    <div className='bg-[#274632] h-screen'>
+    <div className='bg-[#274632] h-screen DashBG'>
       <NavbarComponent/>
       <div className="background w-auto  flex">
          <div className="w-[400px] mx-auto mt-[69px] lg:mt-[74px]">
