a39a87d1a52fa19f900a12b6ec6654e67c0ddca6
mkheck13 <mkheck13@gmail.com>
2024-05-17 11:19:38 -0700

    profile work

--- src/app/Profile/Page.tsx
+++ src/app/Profile/Page.tsx
@@ -1,11 +1,15 @@
 import NavbarComponent from '@/Components/NavBarComponent'
 import React from 'react'
 import '@/app/Profile/styles.css'
+import ProfilePageComponent from '@/Components/ProfilePageComponent'
 
 const Page = () => {
   return (
     <div className='h-[100vh] DashBG'>
         <NavbarComponent/>
+        <div className="w-[400px] mx-auto mt-[69px] lg:mt-[74px]">
+          <ProfilePageComponent />
+        </div>
         
     </div>
   )
